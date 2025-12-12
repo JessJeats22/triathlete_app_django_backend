ae2c6ab201b83656cee364cf902b7f704544b7a7
JessieJ <jvwiltshire22@gmail.com>
2025-12-12 13:05:06 +0000

    feat(trails): add POST support for creating new trails
    Added POST handling to the trail API endpoint to allow creation of new
    Trail records via the API. Validated incoming request data using the
    TrailSerializer and persisted new instances using serializer.save().
    
    The endpoint now returns the serialized trail data with a 201 Created
    response upon successful creation, completing basic write support for
    the trails resource.

--- trails/views.py
+++ trails/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Trail
+from .serializers.common import TrailSerializer
 
 
 
@@ -8,7 +9,15 @@ from .models import Trail
 class TrailShowView(APIView):
         
         
+    # INDEX ROUTE
     def get(self, reqeust):
         trails = Trail.objects.all()
-        print(trails)
-        return Response({'detail': 'HIT TRAIL INDEX ROUTE'})
+        serializer = TrailSerializer(trails, many=True)
+        return Response(serializer.data)
+    
+     # POST ROUTE
+    def post(self, request):
+        serializer = TrailSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, 201)
